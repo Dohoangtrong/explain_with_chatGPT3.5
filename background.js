f5e5521bc662c8a593cd0ccbeba31b10899cc602
FooFoo <105576582+Dohoangtrong@users.noreply.github.com>
2023-06-03 14:08:07 +0700

    Update background.js

--- background.js
+++ background.js
@@ -7,10 +7,8 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(function(info,tab){
     if (info.menuItemId === "highlightAndGetData"){
-        // -----bug?-----
         chrome.tabs.query({active: true, currentWindow: false}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, {text: info.selectionText});
         });
-        // -------
     }
 });
